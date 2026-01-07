3545b89c4b16631da3b5477160d94d80b266724d
二叉树树 <acofork@qq.com>
2026-01-08 00:11:46 +0800

    refactor(astro.config): 简化重定向配置，仅保留必要条目
    移除不再使用的第三方服务重定向链接，仅保留/sponsors的重定向配置，使配置文件更简洁易维护

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,21 +39,7 @@ export default defineConfig({
 	trailingSlash: "always",
 	output: "static",
 	redirects: {
-		"/donate": "/sponsors",
-		"/ak":
-			"https://akile.io/register?aff_code=503fe5ea-e7c5-4d68-ae05-6de99513680e",
-		"/kook": "https://kook.vip/K29zpT",
-		"/long":
-			"https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.in/",
-		"/mly": "https://muleyun.com/aff/GOTRJLPN",
-		"/tg": "https://t.me/+_07DERp7k1ljYTc1",
-		"/tit": "/posts/pin/",
-		"/tly": "https://tianlicloud.cn/aff/HNNCFKGP",
-		"/wly": "https://wl.awcmam.com/#/register?code=FNQwOQBM",
-		"/yyb": "https://www.rainyun.com/acofork_?s=bilibili",
-		"/iku": "https://ikuuu.de/auth/register?code=Bjou",
-		"/esa":
-			"https://tianchi.aliyun.com/specials/promotion/freetier/esa?taskCode=25254&recordId=c856e61228828a0423417a767828d166",
+		"/donate": "/sponsors"
 	},
 	integrations: [
 		tailwind({
